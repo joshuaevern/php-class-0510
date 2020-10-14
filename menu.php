7b23fac7ea278127b953acd42542e1c6c54ff38b
joshuaevern <everncamfed@gmail.com>
2020-10-15 00:07:54 +0200

    updates

--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@ $home = new Menu_Item("Home","/",3,"Opening Screen");
 $dashboard = new Menu_Item("Dashboard","dashboard.php",3,"Your Dashboard");
 $mark  = new Menu_Item("Mark","mark_attendance.php",3,"Mark Attendance");
 $past  = new Menu_Item("Past","attendance_log.php",3,"Past Attendance");
-$Pull_Down_Menu_Item = new Pull_Down_Menu_Item("Manage",[new Menu_Item("Add Student","add_student.php",2,"Add A student to the system");
+$Pull_Down_Menu_Item = new Pull_Down_Menu_Item("Manage",[new Menu_Item("Add Student","add_student.php",2,"Add A student to the system")[
 $Menu_Item= new Menu_Item("Sessions","manage_sessions.php",2,"Add, Remove, Edit upcoming class sessions")
     ],2,"Manage Class");
 $Menu_Item =  new Menu_Item("Logout","../login/logout.php",3,"Logout of system");
